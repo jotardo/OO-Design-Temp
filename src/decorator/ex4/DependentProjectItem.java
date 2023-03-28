a089641d05187708ab67f392e2a33457d6b7a4a8
Sinhvien <Sinhvien@WS316>
2023-03-28 14:20:22 +0700

    forgor

--- src/decorator/ex4/DependentProjectItem.java
+++ src/decorator/ex4/DependentProjectItem.java
@@ -1,5 +1,9 @@
 package decorator.ex4;
 
-public class DependentProjectItem {
+public class DependentProjectItem extends ProjectDecorator{
+
+	public DependentProjectItem(ProjectItem projectItem) {
+		super(projectItem);
+	}
 
 }
